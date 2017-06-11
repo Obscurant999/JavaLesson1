4529ea20c6ee2e392ad0dd38bc7f47a52ff4abcd
Obscurant999 <alexey.kozyrev@gmail.com>
2017-06-11 22:54:59 +0300

    Initial commit

---
+++ src/ArrayInit.java
@@ -0,0 +1,45 @@
+/**
+ * Created by kano on 11.06.17.
+ */
+import java.util.Random;
+
+public class ArrayInit {
+
+    //int arrayL;
+    int min;
+    int max;
+    int[] blackBox;
+
+    ArrayInit(int arrayL) {
+        int[] blackBox = new int[arrayL];
+        Random random = new Random();
+        System.out.println("Содержимое массива: ");
+        for (int i = 0; i < blackBox.length; i++) {
+            blackBox[i] = random.nextInt(999);
+            System.out.print(blackBox[i] + ", ");
+        }
+
+        System.out.println("");
+
+        int min = blackBox[0];
+        for (int j = 0; j < blackBox.length; j++) {
+            if (min > blackBox[j]) {
+            min = blackBox[j];
+            }
+
+        }
+        System.out.println("Наименьшее введенное число : " + min);
+
+        int max = blackBox[0];
+        for (int k = 0; k < blackBox.length; k++) {
+            if (max < blackBox[k]) {
+                max = blackBox[k];
+            }
+        }
+        System.out.println("Наибольшее введенное число : " + max);
+
+
+    }
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/**
+ * Created by kano on 11.06.17.
+ */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Задайте длину массива (от 1 до 100):");
+        int arrayL = in.nextInt();
+        if (arrayL <= 0) {
+            System.out.println("Ошибка! Длина массива должна быть от 1 до 100!");
+        } else if (arrayL > 100) {
+            System.out.println("Ошибка! Длина массива должна быть от 1 до 100!");
+        } else {
+            ArrayInit array = new ArrayInit(arrayL);
+
+        }
+
+    }
+}
